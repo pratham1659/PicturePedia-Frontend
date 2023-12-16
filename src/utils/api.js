5486467c2b6fc46691363e04b8c98c8567d832f8
prathamkumar <prathamkumar1985@gmail.com>
2023-12-16 23:47:26 +0530

    Project Implimentation

---
+++ src/utils/api.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { url } from "inspector";
+
+const BASE_URL = "https://api.themoviedb.org/3";
+
+const TMBD_TOKEN = import.meta.env.VITE_APP_TMBD_TOKEN;
+
+const headers = {
+  Authorization: "bearer " + TMBD_TOKEN,
+};
+
+export const fetchDataFromApi = async (url, params) => {
+  try {
+    const { data } = await axios.get(BASE_URL + url, {
+      headers,
+      params,
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
