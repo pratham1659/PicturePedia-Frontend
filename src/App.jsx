ae1a250665932138cd1d289a543b2fb9ebdff04e
prathamkumar <prathamkumar1985@gmail.com>
2023-12-17 01:11:09 +0530

    api testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import { useState } from "react";
+import { useEffect } from "react";
+import { fetchDataFromApi } from "./utils/api";
 
 function App() {
-  return (
-    <>
-      <div></div>
-    </>
-  );
+  useEffect(() => {
+    apiTesting();
+  }, []);
+
+  const apiTesting = () => {
+    fetchDataFromApi("/movie/popular").then((res) => {
+      console.log(res);
+    });
+  };
+
+  return <div className="App">App</div>;
 }
 
 export default App;
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,12 +1,11 @@
 import axios from "axios";
-import { url } from "inspector";
 
 const BASE_URL = "https://api.themoviedb.org/3";
-
-const TMBD_TOKEN = import.meta.env.VITE_APP_TMBD_TOKEN;
+const TMDB_TOKEN =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YjY4ZDMxYWIzZmI4YjE5YjQ5ZWUxZDMzNmU4N2RlOCIsInN1YiI6IjY1N2NhMTkyZWEzOTQ5MDBmZWIwZmM3MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.htFEYczPXlGbj_z2xgnUS3VKavlCgELgFxnI0BM_W3w";
 
 const headers = {
-  Authorization: "bearer " + TMBD_TOKEN,
+  Authorization: "bearer " + TMDB_TOKEN,
 };
 
 export const fetchDataFromApi = async (url, params) => {
@@ -15,9 +14,9 @@ export const fetchDataFromApi = async (url, params) => {
       headers,
       params,
     });
-
     return data;
-  } catch (error) {
-    console.log(error);
+  } catch (err) {
+    console.error("getJobs error:", err);
+    return err;
   }
 };
