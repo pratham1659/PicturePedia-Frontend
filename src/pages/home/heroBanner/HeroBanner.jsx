75ee260637dd447b46285137e7b472c5a456d6ae
prathamkumar <prathamkumar1985@gmail.com>
2023-12-21 22:30:27 +0530

    Final Touchup CSS

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -39,7 +39,7 @@ const HeroBanner = () => {
       <div className="opacity-layer"></div>
       <ContentWrapper>
         <div className="heroBannerContent">
-          <span className="title">Welcome to PicturePedia</span>
+          <span className="title">Welcome</span>
           <span className="subTitle">
             Here You can find your Favourites Millions of movies, TV shows and
             people to discover. Explore now.
